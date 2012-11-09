dd6648044fdb673c51b2a06c884cd26997b122d8
Marc Verhagen <marc@cs.brandeis.edu>
2012-11-08 21:39:58 -0500

    Factored out batch utils for ontology creation and matcher.

--- batch.py
+++ batch.py
@@ -1,5 +1,11 @@
+"""
+
+Various utilities for ontology creation and pattern matcher.
+
+"""
 
 def read_stages(target_path, language):
+    """Read the counts in target_path/language/ALL_STAGES.txt."""
     stages = {}
     for line in open(os.path.join(target_path, language, 'ALL_STAGES.txt')):
         if not line.strip():
@@ -9,17 +15,18 @@ def read_stages(target_path, language):
     return stages
 
 def update_stages(target_path, language, stage, limit):
-    """Updates the counts in ALL_STAGES.txt. This includes rereading the file because
-    during processing on one machine another machine could have done some other processing
-    and have updated the fiel, we do not want to lose those updates. This could
-    potentially go wrong when two separate processes terminate at the same time, a rather
-    unlikely occurrence."""
+    """Updates the counts in target_path/language/ALL_STAGES.txt. This includes rereading
+    the file because during processing on one machine another machine could have done some
+    other processing and have updated the fiel, we do not want to lose those updates. This
+    could potentially go wrong when two separate processes terminate at the same time, a
+    rather unlikely occurrence."""
     stages = read_stages(target_path, language)
     stages.setdefault(stage, 0)
     stages[stage] += limit
     write_stages(target_path, language, stages)
     
 def write_stages(target_path, language, stages):
+    """Write stages counts to target_path/language/ALL_STAGES.txt."""
     stages_file = os.path.join(target_path, language, 'ALL_STAGES.txt')
     backup_file = os.path.join(target_path, language,
                                "ALL_STAGES.%s.txt" % time.strftime("%Y%m%d-%H%M%S"))
