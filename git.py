8f89c85cd956208e717defd14080f5e9ebfcfca3
Marc Verhagen <marc@cs.brandeis.edu>
2013-02-12 16:45:45 -0500

    Processing chain redesign for step 2.

--- git.py
+++ git.py
@@ -1,10 +1,12 @@
-import subprocess
+from subprocess import Popen, PIPE
 
 
 def get_git_commit():
 
     try:
-        commit = subprocess.check_output(['git', 'describe']).strip()
+        commit = Popen(["git", "describe"], stdout=PIPE).communicate()[0].strip()
+        # the following only works in Python 2.7
+        # commit = subprocess.check_output(['git', 'describe']).strip()
         return commit
     except OSError:
         return "unknown"
